c3dddcf47861be095dd14298c51188f9b022fcf5
Nathan Wildofsky <nsw7227@rit.edu>
2020-11-04 21:24:05 -0500

    Checkoff #1
    Implemented seeking, fleeing, collisions, debug lines, and an arrival behavior fro the zombies after all the humans are dead

---
+++ Wildofsky_HvZ/Assets/Scripts/Treasure.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Treasure : MonoBehaviour
+{
+    public void OnGrab()
+    {
+        MeshRenderer floor = GameObject.FindWithTag("Floor").GetComponent<MeshRenderer>();
+        Vector3 max = floor.bounds.max;
+        Vector3 min = floor.bounds.min;
+
+        // When the treasure is grabbed by the human, give it a new random location within the floor object
+        transform.position = new Vector3(Random.Range(min.x, max.x), GetComponent<MeshRenderer>().bounds.extents.y, Random.Range(min.z, max.z));
+    }
+}
